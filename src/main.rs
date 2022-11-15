586520eabf8e6e6a010580b7a5447ae44b391bcb
Appricot144 <aniline.96.256@gmail.com>
2022-11-15 19:32:58 +0900

    collect - .tar.gzの展開先を引数で指定したディレクトリにしました 		- tar -v optionを抜きました
    download - DLの開始エントリ start= を指定できるようにしました
    		 	usage: download_all_cookbook --start 840

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use reqwest::Client;
 use serde::Deserialize;
 use tokio::process::Command;
 //use std::process::Stdio;
+use std::env;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 const SIZE: i32 = 10;
@@ -21,22 +22,30 @@ struct SuperResponse {
 	items: Vec<Cookbook>
 }
 
+// DLするtar-ballのfile pathをパースするが面倒なのでknife supermarket コマンドを実行している。
+// <apiからDLする方法>
+// GET /api/v1/cookbooks/COOKBOOK_NAME/versions/latestのレスポンスに
+// "file": "<file path>" tar-ballのファイルパスがある
+// このURIにgetすると多分amazonawsにリダイレクトするので-Lオプションをつけて
+// curl -X "GET" -L "<file_path>" -o cookbook_name.tar.gz 
 #[tokio::main]
 async fn main() -> Result<()> {
+	let args: Vec<String> = env::args().collect(); // download_all_cookbook --start 10 	
+	if args.len() < 3 { panic!("no start-option \"--start NUM\""); }
+	
 	let client = Client::new();	
 	let mut url = "https://supermarket.chef.io/api/v1/cookbooks".to_string();
 	let response = client.get(&url).send().await?;
 	let response = response.json::<SuperResponse>().await?;
 
-	let mut start = 0;
+	let mut start: i32 = args[2].parse()?;
 	let total = response.total;
-	url.push_str("?item=10&start=");
+	url.push_str("?items=10&start=");
 
 	loop {
-		// url set
-		let num_str = start.to_string(); 
+		// set url
+		let num_str = start.to_string();
 		let add_url = url.clone() + &num_str;
-		//println!("{}", add_url);
 		
 		let response = client.get(&add_url).send().await?;
 		let response = response.json::<SuperResponse>().await?;
@@ -55,7 +64,7 @@ async fn main() -> Result<()> {
 			}
 		}
 
-		// index set
+		// set index
 		start = response.start + SIZE;
 
 		if start >= total { break }
